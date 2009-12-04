9a3da9981ef12927e0a9f9e11eedbc998b9abccd
schutz.michael <schutz.michael@e50b18a8-a9ce-11de-bea2-1db044c32224>
2009-12-04 18:16:57 +0000

    Renamed target folder and deployed file name

--- src/SI/build/setup-si.py
+++ src/SI/build/setup-si.py
@@ -8,10 +8,10 @@ from distutils.core import setup
 import sys, os
 
 
-distDir=os.path.join(os.environ.get('HOME'), "Desktop/SensoryIntegrationExperiment")
+distDir=os.path.join(os.environ.get('HOME'), "Desktop/Auditory Experiments")
 
 setupOpts = {
-    'name' : 'Sensory Integration Experiment',
+    'name' : 'Aud Research Suite',
     'description' : 'Perception Experiements, PI: Michael Schutz',
     'version' : '40',
     'author' : 'Simeon H.K. Fitch',
@@ -29,7 +29,7 @@ if sys.platform == 'darwin':
             # and make sure the CFBundleIcon plist key is set appropriately.
             'iconfile': '../resources/MustardSeedIcon.icns',
             'dist_dir' : distDir,        
-            'packages': ['wx', 'wxPython']    
+            'packages': ['wx', 'wxPython','ctypes']    
         },
 #        'bdist-base' : ['/tmp/build-si']
     }
@@ -43,7 +43,7 @@ if sys.platform == 'darwin':
     text = 'To install, drag application to another folder'
     f = open(os.path.join(distDir, text), 'w')
     f.close()
-    os.system('hdiutil create -ov -format UDZO -scrub -imagekey zlib-level=9 -srcfolder ' + distDir + ' ' + distDir + '.dmg')
+    os.system('hdiutil create -ov -format UDZO -scrub -imagekey zlib-level=9 -srcfolder "' + distDir + '" "' + distDir + '.dmg"')
     print 'Deleting build directory...'
     import shutil
     shutil.rmtree('./build', ignore_errors=True)
