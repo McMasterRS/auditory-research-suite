f6be21dbd1061c1035da27c9fd2682b38e5eba3b
cje <cje@datamininglab.com>
2011-07-20 13:55:24 +0000

    created TOJ classes

--- src/java/edu/mcmaster/maplelab/toj/TOJSetupScreen.java
+++ src/java/edu/mcmaster/maplelab/toj/TOJSetupScreen.java
@@ -6,36 +6,85 @@ import edu.mcmaster.maplelab.common.datamodel.Session;
 import edu.mcmaster.maplelab.common.gui.SimpleSetupScreen;
 import edu.mcmaster.maplelab.toj.datamodel.TOJSession;
 
+import javax.swing.*;
+
+
 public class TOJSetupScreen extends SimpleSetupScreen<TOJSession> {
 	/** Experiment basename - placed here temporarily */
 	public static final String EXPERIMENT_BASENAME = "Temporal Order Judgment";
 
+	
+	// new text fields
+	private JFormattedTextField _screenWidth;
+	private JFormattedTextField _screenHeight;
+	private JFormattedTextField _dataFileName;
+	private JCheckBox _includeVideoStimuli;
+	
 	public TOJSetupScreen(String prefsPrefix) {
 		super(EXPERIMENT_BASENAME.replace(" ", "").toLowerCase(), true);
 	}
 
 	@Override
 	protected void addExperimentFields() {
-		// TODO Auto-generated method stub
+		// where on screen do they go?
 		
+		addLabel("Screen width");
+		_screenWidth = new JFormattedTextField();
+		_screenWidth.setValue(720);
+		addField(_screenWidth);
+		
+		addLabel("Screen height");
+		_screenHeight = new JFormattedTextField();
+		_screenHeight.setValue(480);
+		addField(_screenHeight);
+		
+		addLabel("Data file name");
+		_dataFileName = new JFormattedTextField();
+		_dataFileName.setValue("TemporalOrderJudgmentTrialData.txt");
+		addField(_dataFileName);
+		
+		addLabel("Include video stimuli");
+		_includeVideoStimuli = new JCheckBox();
+		addField(_includeVideoStimuli);		
 	}
 
 	@Override
 	protected void applyExperimentSettings(TOJSession session) {
-		// TODO Auto-generated method stub
-		
+		session.setScreenWidth(Integer.parseInt(_screenWidth.getText()));
+		session.setScreenHeight(Integer.parseInt(_screenHeight.getText()));
+		session.setDataFileName(_dataFileName.getText());
+		session.includeVisualStimuli(_includeVideoStimuli.isSelected());
 	}
 
 	@Override
 	protected void putExperimentPrefs(Preferences prefs) {
-		// TODO Auto-generated method stub
+		int prefScreenWidth = Integer.parseInt(_screenWidth.getText());
+		prefs.putInt(TOJSession.ConfigKeys.screenWidth.name(), prefScreenWidth);
+		
+		int prefScreenHeight = Integer.parseInt(_screenHeight.getText());
+		prefs.putInt(TOJSession.ConfigKeys.screenHeight.name(), prefScreenHeight);
+		
+		String prefDataFileName = _dataFileName.getText();
+		prefs.put(TOJSession.ConfigKeys.dataFileName.name(), prefDataFileName);
+		
+		boolean prefIncludeVideoSimuli = _includeVideoStimuli.isSelected();
+		prefs.putBoolean(TOJSession.ConfigKeys.includeVisualStimuli.name(), prefIncludeVideoSimuli);
 		
 	}
 
 	@Override
 	protected void loadExperimentPrefs(Preferences prefs) {
-		// TODO Auto-generated method stub
+		int prefScreenWidth = prefs.getInt(TOJSession.ConfigKeys.screenWidth.name(), 720);
+		_screenWidth.setValue(new Integer(prefScreenWidth));
 		
+		int prefScreenHeight = prefs.getInt(TOJSession.ConfigKeys.screenHeight.name(), 480);
+		_screenHeight.setValue(new Integer(prefScreenHeight));
+		
+		String prefDataFileName = prefs.get(TOJSession.ConfigKeys.dataFileName.name(), "TemporalOrderJudgmentTrialData.txt");
+		_dataFileName.setValue(prefDataFileName);
+		
+		boolean prefIncludeVideoStimuli = prefs.getBoolean(TOJSession.ConfigKeys.includeVisualStimuli.name(), true);
+		_includeVideoStimuli.setSelected(new Boolean(prefIncludeVideoStimuli));
 	}
 
 }
--- src/java/edu/mcmaster/maplelab/toj/datamodel/TOJSession.java
+++ src/java/edu/mcmaster/maplelab/toj/datamodel/TOJSession.java
@@ -6,7 +6,53 @@ import java.util.Properties;
 import edu.mcmaster.maplelab.common.datamodel.Session;
 
 public class TOJSession extends Session<TOJBlock, TOJTrial, L> {
+	
+	
+	public enum ConfigKeys {
+		screenWidth,
+		screenHeight,
+		dataFileName,
+		includeVisualStimuli
+	}
+	
+	// setters
+	public void setScreenWidth(int width) {
+		setProperty(ConfigKeys.screenWidth, width);
+	}
+	
+	public void setScreenHeight(int height) {
+		setProperty(ConfigKeys.screenHeight, height);
+	}
 
+	public void setDataFileName(String name) {
+		setProperty(ConfigKeys.dataFileName, name);
+	}
+	
+	public void includeVisualStimuli(boolean include) {
+		setProperty(ConfigKeys.includeVisualStimuli, include);
+	}
+	
+	
+	// getters
+	public int getScreenWidth() {
+		return getInteger(ConfigKeys.screenWidth, 720);
+	}
+	
+	public int getScreenHeight() {
+		return getInteger(ConfigKeys.screenHeight, 480);
+	}
+	
+	public String getDataFileName() {
+		return getString(ConfigKeys.dataFileName);
+	}
+	
+	public boolean includeVisualStimuli() {
+		return getBoolean(ConfigKeys.includeVisualStimuli, true);
+	}
+	
+	
+	
+	
 	protected TOJSession(Properties props) {
 		super(props);
 		// TODO Auto-generated constructor stub
