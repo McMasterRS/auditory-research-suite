011c080a7fdfa2226cf2d29445a2dfb4d7dc4f05
ben.guseman <ben.guseman@mseedsoft.com>
2011-07-20 15:30:23 +0000

    added animator panel

---
+++ src/java/edu/mcmaster/maplelab/common/gui/AnimatorPanel.java
@@ -0,0 +1,119 @@
+package edu.mcmaster.maplelab.common.gui;
+
+import javax.swing.JPanel;
+
+import java.awt.*;
+import java.io.File;
+import java.util.*;
+import java.util.List;
+
+import javax.media.opengl.*;
+import javax.swing.*;
+import javax.vecmath.Vector3d;
+
+import com.sun.opengl.util.Animator;
+
+import edu.mcmaster.maplelab.common.LogContext;
+
+public class AnimatorPanel extends JPanel {
+	private final GLJPanel _canvas;
+	private final Animator _animator;
+    
+    static {
+        // Put JOGL version information into system properties to 
+        // assist in debugging.
+        Package joglPackage = Package.getPackage("javax.media.opengl");
+        System.setProperty("jogl.specification.version", joglPackage.getSpecificationVersion());
+        System.setProperty("jogl.implementation.version", joglPackage.getImplementationVersion());
+        
+        ToolTipManager.sharedInstance().setLightWeightPopupEnabled(false);
+    }
+    
+    /** 
+     * Default ctor.
+     */
+    public AnimatorPanel() {
+        super(new BorderLayout());
+        setBorder(UIManager.getDefaults().getBorder("ProgressBar.border"));
+
+        GLCapabilities caps = new GLCapabilities();
+
+        _canvas = new GLJPanel(caps);
+        _canvas.setName("glCanvas");
+        _animator = new Animator();
+        _canvas.addGLEventListener(_animator);
+        
+        add(_canvas, BorderLayout.CENTER);
+        setPreferredSize(new Dimension(640, 480));
+        
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.indexOf("mac") >= 0 || osName.indexOf("darwin") >= 0) {
+        	// Without this call we get a canvas width or height == 0 error.
+            _canvas.setSize(getPreferredSize());
+        }
+    }
+    
+    /**
+     * {@inheritDoc} 
+     * @see java.awt.Component#setCursor(java.awt.Cursor)
+     */
+    @Override
+    public void setCursor(Cursor cursor) {
+        super.setCursor(cursor);
+        _canvas.setCursor(cursor);
+    }
+
+    /**
+     * Attempt to make errors generated when 3D not available a little nicer.
+     * {@inheritDoc} 
+     * @see java.awt.Component#addNotify()
+     */
+    @Override
+    public void addNotify() {
+        try {
+            super.addNotify();
+        }
+        catch (UnsatisfiedLinkError ex) {
+        	LogContext.getLogger().warning("Sorry, 3D model rendering has not been enabled " +
+        			"for this platform.");
+        	ex.printStackTrace();
+        }
+    } 
+
+    /**     
+     * {@inheritDoc} 
+     * @see java.awt.Component#repaint()
+     */
+    @Override
+    public void repaint() {
+        super.repaint();
+        // Force a repaint on GLCanvas.
+        if(_canvas != null) {
+            // NB: canvas might be null during component instantiation due
+            // to some brain dead code in swing.
+            _canvas.repaint();
+        }
+    }
+
+    public void repaintLater() {
+        //invalidate();
+        EventQueue.invokeLater(new Runnable() {
+           public void run() {
+               /*_canvas.invalidate();
+               getParent().invalidate();
+               getParent().validate();
+               _canvas.validate();*/
+               repaint();
+           }
+        });
+    }
+    
+    /**
+     * Overridden to provide appropriate printing behavior.
+     */
+    @Override
+    public void print(Graphics g) {
+        _canvas.display();
+        super.print(g);
+    }
+}
