acf04c2a0468afab65987429863bd2a367bf8b4b
mudwayt <mudwayt@mcmaster.ca>
2021-03-19 18:54:33 -0400

    Reworked setup interface to request properties file

--- src/python3/Interfaces/ExperimentSetup.py
+++ src/python3/Interfaces/ExperimentSetup.py
@@ -24,10 +24,10 @@ class ExperimentSetup(QtWidgets.QWidget):
         self.done = False
 
         # Setup UI
-        uic.loadUi(getGui('TOJSetup.ui'), self)
-        # self.tbDataDirectory.setText(os.getcwd())
-        self.btBrowse.clicked.connect(lambda: self.browseDataDirectory("data"))
-        self.btBrowseProperties.clicked.connect(lambda: self.browseDataDirectory("prop"))
+        uic.loadUi(getGui('Setup.ui'), self)
+        self.tbDataDirectory.setText(os.getcwd())
+        self.btBrowse.clicked.connect(self.browseDataDirectory)
+        self.btBrowseProperties.clicked.connect(self.browseFile)
         self.rbDataDir.toggled.connect(self.toggleProperties)
         self.btStart.clicked.connect(self.startExperiment)
         self.btCancel.clicked.connect(self.close)
@@ -39,12 +39,13 @@ class ExperimentSetup(QtWidgets.QWidget):
         event.accept()
 
     # Open the directory browser
-    def browseDataDirectory(self, dirType):
+    def browseDataDirectory(self):
         dataDir = QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory")
-        if dirType == "data":
-            self.tbDataDirectory.setText(dataDir)
-        else:
-            self.tbPropertiesFile.setText(dataDir)
+        self.tbDataDirectory.setText(dataDir)
+
+    def browseFile(self):
+        f = QtWidgets.QFileDialog.getOpenFileName(self, "Open Properties File", ".", "Properties File (*.properties)")[0]
+        self.tbPropertiesFile.setText(f)
 
     def toggleProperties(self):
         self.tbPropertiesFile.setEnabled(not self.rbDataDir.isChecked())
@@ -55,19 +56,21 @@ class ExperimentSetup(QtWidgets.QWidget):
 
         # Generate properties filename based on the selected radio button
         if self.rbDataDir.isChecked():
-            propFile, exists = mergePathsVerify([self.tbDataDirectory.text(), self.cbProperties.currentText()])
+            for filename in os.listdir(self.tbDataDirectory.text()):
+                if os.path.splitext(filename)[1] == ".properties":
+                    propFile = os.path.join(path, filename)
         else:
-            propFile, exists = mergePathsVerify([self.tbPropertiesFile.text(), self.cbProperties.currentText()])
+            propFile = self.tbPropertiesFile.text()
 
         # Check if the properties file exists
-        if not exists:
-            msgbox = QtWidgets.QMessageBox.critical(self, "Error - File Not Found", "Unable to locate file {0}".format(propFile))
+        if not os.path.exists(propFile):
+            msgbox = QtWidgets.QMessageBox.critical(self, "Error - File Not Found", "Unable to locate properties file")
             return
 
         # Check which properties file to open and make sure it can be parsed
         if self.cbProperties.currentText() == "toj.properties":
             e, properties = readTojProperties(propFile)
-        elif self.cbProperties.currentText() == "AV7B-si.properties":
+        elif self.cbProperties.currentText() == "si.properties":
             e, properties = readSIProperties(propFile)
 
         if not e:
