8ac4a790681ee90deb79ad03613ec1a90c0e3e03
mudwayt <mudwayt@mcmaster.ca>
2021-03-20 17:51:54 -0400

    Updating setup.py

--- src/python3/setup.py
+++ src/python3/setup.py
@@ -7,12 +7,18 @@ with open("./MusicalIllusion/_version.py", "w") as f:
     f.write('__version__ = "{0}"\n'.format(version))
     f.write('__versionDate__ = "{0}"'.format(datetime.date.today().strftime("%d/%m/%Y")))
 
+with open("README", "r") as fh:
+    long_description = fh.read()
+
 
 setuptools.setup(
     name="Auditory Research Suite",
     version=version,
     author="Thomas Mudway, Michael Schutz",
     packages=setuptools.find_packages(),
+    description="A suite of research tools designed for investigating auditory illusions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     package_data={
         "": ["*.ui"],
     },
